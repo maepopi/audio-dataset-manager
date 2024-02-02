6d481165584c3be7c140b35ca2471562c9b98163
Dr. Jusseaux <35258413+maepopi@users.noreply.github.com>
2024-02-02 22:54:36 +0100

    attached the check_json tab to main ui

---
+++ src/check_json_ui.py
@@ -0,0 +1,64 @@
+import gradio as gr
+import os
+import json
+import check_json_utils as utils
+
+
+handler = utils.AudioJsonHandler()
+
+
+def create_check_json_interface():
+    with gr.Blocks() as interface:
+            json_folder = gr.Textbox(label='Path to your retranscription json and clips')
+            submit_button = gr.Button('Load') # This is supposed to load the audios, but also the json file and the ebook if they exist
+
+
+            process_group = gr.Group(visible=True)
+
+            with process_group:
+                with gr.Row():
+                    audio_player = gr.Audio()
+                    audio_name_box = gr.Textbox(label='Audio File Name', interactive=True)
+
+
+                with gr.Row():
+                    previous_audio_btn = gr.Button('Previous')
+                    delete_audio = gr.Button('Delete from dataset')
+                    next_audio_btn = gr.Button('Next')
+
+                with gr.Row(equal_height=True):
+
+                    json_reference = gr.Textbox(label='JSON reference', scale=20, interactive=True)
+                    save_json_button = gr.Button('Save JSON')
+
+                with gr.Row():
+                    epub_reference = gr.Textbox(label='EPUB reference')
+
+
+                current_page_label = gr.Label('Current page : 1/X')
+                page_input = gr.Number(label='Enter page', value=1)
+                go_button = gr.Button('Go to page')
+                info_textbox = gr.Markdown(visible=True)
+
+                submit_button.click(fn=handler.load_and_init, 
+                                    inputs = [json_folder], 
+                                    outputs = [audio_player, audio_name_box, page_input, current_page_label, json_reference] ) 
+                
+                next_audio_btn.click(fn=lambda index, json_folder: handler.handle_pagination(index, json_folder, 1), 
+                                    inputs=[page_input, json_folder], 
+                                    outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference])
+                
+                previous_audio_btn.click(fn=lambda index, json_folder: handler.handle_pagination(index, json_folder, -1), 
+                                        inputs=[page_input, json_folder], 
+                                        outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference])
+                
+                go_button.click(fn=lambda index, json_folder: handler.change_audio(index - 1, json_folder), 
+                                inputs=[page_input, json_folder], 
+                                outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference])
+                
+
+                save_json_button.click(fn=handler.save_json, inputs=[json_reference, audio_name_box], outputs=info_textbox)
+        
+
+
+    return interface
--- webui_main.py
+++ webui_main.py
@@ -11,7 +11,7 @@ sys.path.append(src_folder)
 from analyze_ui import create_analyze_audio_interface
 from split_ui import create_split_audio_interface
 from transcribe_ui import create_transcribe_audio_interface
-from label_check_ui import create_label_check_interface
+from check_json_ui import create_check_json_interface
 
 
 
@@ -22,7 +22,7 @@ from label_check_ui import create_label_check_interface
 analyze_audio_ui = create_analyze_audio_interface()
 split_audio_ui = create_split_audio_interface()
 transcribe_audio_ui = create_transcribe_audio_interface()
-label_check_ui = create_label_check_interface()
+label_check_ui = create_check_json_interface()
 
 interfaces = [analyze_audio_ui, split_audio_ui, transcribe_audio_ui, label_check_ui]
 tab_names = ["Analyze audio", "Split audio", "Transcribe audio", "Label and check dataset"]
