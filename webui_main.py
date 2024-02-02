6d70e34a23b7bbffd752e273c836fc6ce3cbfe29
Dr. Jusseaux <35258413+maepopi@users.noreply.github.com>
2024-02-02 22:55:13 +0100

    renamed tab

--- webui_main.py
+++ webui_main.py
@@ -15,17 +15,13 @@ from check_json_ui import create_check_json_interface
 
 
 
-
-
-
-
 analyze_audio_ui = create_analyze_audio_interface()
 split_audio_ui = create_split_audio_interface()
 transcribe_audio_ui = create_transcribe_audio_interface()
 label_check_ui = create_check_json_interface()
 
 interfaces = [analyze_audio_ui, split_audio_ui, transcribe_audio_ui, label_check_ui]
-tab_names = ["Analyze audio", "Split audio", "Transcribe audio", "Label and check dataset"]
+tab_names = ["Analyze audio", "Split audio", "Transcribe audio", "Check JSON and audios"]
 
 tabbed_interface = gr.TabbedInterface(interface_list=interfaces, tab_names=tab_names)
 
