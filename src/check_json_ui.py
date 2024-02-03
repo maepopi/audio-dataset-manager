2ce6ac25c4ef797ee8d35a90c6f0dca10f3e1ce4
Dr. Jusseaux <35258413+maepopi@users.noreply.github.com>
2024-02-03 21:15:47 +0100

    implemented segments - prod

--- src/check_json_ui.py
+++ src/check_json_ui.py
@@ -7,56 +7,63 @@ handler = utils.AudioJsonHandler()
 
 def create_check_json_interface():
     with gr.Blocks() as interface:
-            json_folder = gr.Textbox(label='Path to your retranscription json and clips')
-            submit_button = gr.Button('Load') # This is supposed to load the audios, but also the json file and the ebook if they exist
+        json_folder = gr.Textbox(label='Path to your retranscription json and clips')
+        submit_button = gr.Button('Load') # This is supposed to load the audios, but also the json file and the ebook if they exist
+        all_segment_boxes = []
+        total_segment_components = 10
+
+        with gr.Row():
+            audio_player = gr.Audio()
+            audio_name_box = gr.Textbox(label='Audio File Name', interactive=True)
 
 
-            process_group = gr.Group(visible=True)
+        with gr.Row():
+            previous_audio_btn = gr.Button('Previous')
+            delete_audio = gr.Button('Delete from dataset')
+            next_audio_btn = gr.Button('Next')
 
-            with process_group:
-                with gr.Row():
-                    audio_player = gr.Audio()
-                    audio_name_box = gr.Textbox(label='Audio File Name', interactive=True)
+        with gr.Row(equal_height=True):
+
+            json_reference = gr.Textbox(label='JSON reference', scale=20, interactive=True)
+            save_json_button = gr.Button('Save JSON')
+
+        for _ in range(total_segment_components):
+            with gr.Row():
+                seg_textbox = gr.Textbox(visible=False, scale=50)
+                seg_start = gr.Textbox(visible=False)
+                seg_end = gr.Textbox(visible=False)
+
+            all_segment_boxes.extend((seg_textbox, seg_start, seg_end))
+
+        with gr.Row():
+            epub_reference = gr.Textbox(label='EPUB reference')
 
 
-                with gr.Row():
-                    previous_audio_btn = gr.Button('Previous')
-                    delete_audio = gr.Button('Delete from dataset')
-                    next_audio_btn = gr.Button('Next')
+        current_page_label = gr.Label('Current page : 1/X')
+        page_input = gr.Number(label='Enter page', value=1)
+        go_button = gr.Button('Go to page')
+        info_textbox = gr.Markdown(visible=True)
 
-                with gr.Row(equal_height=True):
+        submit_button.click(fn=lambda json_folder: handler.load_and_init(json_folder, total_segment_components), 
+                            inputs = [json_folder], 
+                            outputs = [audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox, *all_segment_boxes] ) 
 
-                    json_reference = gr.Textbox(label='JSON reference', scale=20, interactive=True)
-                    save_json_button = gr.Button('Save JSON')
+        next_audio_btn.click(fn=lambda index, json_folder: handler.handle_pagination(index, json_folder, 1, total_segment_components), 
+                            inputs=[page_input, json_folder], 
+                            outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox, *all_segment_boxes])
 
-                with gr.Row():
-                    epub_reference = gr.Textbox(label='EPUB reference')
-
-
-                current_page_label = gr.Label('Current page : 1/X')
-                page_input = gr.Number(label='Enter page', value=1)
-                go_button = gr.Button('Go to page')
-                info_textbox = gr.Markdown(visible=True)
-
-                submit_button.click(fn=handler.load_and_init, 
-                                    inputs = [json_folder], 
-                                    outputs = [audio_player, audio_name_box, page_input, current_page_label, json_reference] ) 
-                
-                next_audio_btn.click(fn=lambda index, json_folder: handler.handle_pagination(index, json_folder, 1), 
-                                    inputs=[page_input, json_folder], 
-                                    outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox])
-                
-                previous_audio_btn.click(fn=lambda index, json_folder: handler.handle_pagination(index, json_folder, -1), 
-                                        inputs=[page_input, json_folder], 
-                                        outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox])
-                
-                go_button.click(fn=lambda index, json_folder: handler.change_audio(index - 1, json_folder), 
+        previous_audio_btn.click(fn=lambda index, json_folder: handler.handle_pagination(index, json_folder, -1, total_segment_components), 
                                 inputs=[page_input, json_folder], 
-                                outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox])
-                
+                                outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox, *all_segment_boxes])
 
-                save_json_button.click(fn=handler.save_json, inputs=[json_folder, json_reference, audio_name_box], outputs=info_textbox)
-        
+        go_button.click(fn=lambda index, json_folder: handler.change_audio(index - 1, json_folder, total_segment_components), 
+                        inputs=[page_input, json_folder], 
+                        outputs=[audio_player, audio_name_box, page_input, current_page_label, json_reference, info_textbox, *all_segment_boxes])
+
+
+        save_json_button.click(fn=handler.save_json, inputs=[json_folder, json_reference, audio_name_box, *all_segment_boxes], outputs=info_textbox)
+
+
 
 
     return interface
--- src/check_json_utils.py
+++ src/check_json_utils.py
@@ -11,41 +11,52 @@ class AudioJsonHandler():
         return next(
         (file for file in os.listdir(path) if file.endswith('.json')), None)
 
-    def load_and_init(self, json_folder):
+    def load_and_init(self, json_folder, total_segment_components):
         json_file = self.get_json(json_folder)
         json_path = os.path.join(json_folder, json_file)
         with open(json_path, 'r') as file:
             self.json_data = json.load(file)
 
-        return self.change_audio(0, json_folder)
+        return self.change_audio(0, json_folder, total_segment_components)
 
-    def save_json(self, json_folder, text, audio_name):
+    def save_json(self, json_folder, text, audio_name, *all_segment_boxes):
+        def process_json(file, text, audio_name, cleaned_textboxes):
+            self.json_data = json.load(file)
+            self.json_data[audio_name]['text'] = text
+            segments = self.json_data[audio_name]['segments']
+
+            for i, segment in enumerate(segments):
+                # The list makes the texts, starts and ends follow by groups of three. We need to parse with a delta.
+                j = i*3
+                segment['text'], segment['start'], segment['end'] = all_segment_boxes[j:j+3] # Each time we're assigning a slice of a list to the right keys
+
+            file.seek(0)
+            json.dump(self.json_data, file, indent=4)
+            file.truncate()
+    
         json_file = self.get_json(json_folder)
         json_file_path = os.path.join(json_folder, json_file)
-
+        cleaned_textboxes= [i for i in all_segment_boxes if i]
+        
 
         with open(json_file_path, 'r+') as file:
-               self.json_data = json.load(file)
-               self.json_data[audio_name]['text'] = text
-               file.seek(0)
-               json.dump(self.json_data, file, indent=4)
-               file.truncate()
-
+            process_json(file, text, audio_name, cleaned_textboxes)
         return '>> *The JSON was saved.*'
 
-    def handle_pagination(self, page, json_folder, delta):
+
+    def handle_pagination(self, page, json_folder, delta, total_segment_components):
         new_index = page -1 + delta # We adjust for zero-based indexing, and the delta determines which way we move
         # Check if the new_index is within the valid range
         if 0 <= new_index < len(self.json_data):
-            return self.change_audio(new_index, json_folder)
+            return self.change_audio(new_index, json_folder, total_segment_components)
         else:
             # If the new_index is out of bounds, return current state without change
             # To achieve this, subtract delta to revert to original page index
-            return self.change_audio(page - 1, json_folder)  # page - 1 adjusts back to zero-based index
+            return self.change_audio(page - 1, json_folder, total_segment_components)  # page - 1 adjusts back to zero-based index
 
  
             
-    def change_audio(self, index, json_folder):
+    def change_audio(self, index, json_folder, total_segment_components):
 
 
         def get_audio_file():
@@ -56,19 +67,53 @@ class AudioJsonHandler():
         def get_audio_text(audio_name):
             return self.json_data[audio_name]['text']
 
+        def create_segment_group(segments):
+            new_segment_group = []
+            visible_segments = len(segments)
 
-        entry_amount = len(self.json_data)
+            for i in range(total_segment_components):
+                visible = i < visible_segments
+                text = segments[i]['text'] if visible else ''
+                start = segments[i]['start'] if visible else ''
+                end = segments[i]['end'] if visible else ''
+
+               
+
+                seg_textbox = gr.Textbox(visible=visible, value=text, label=f'Segment {i+1} Text', interactive=True, scale=50)
+                start_number = gr.Textbox(visible=visible, value=str(start), label=f'Segment {i+1} Start', interactive=True)
+                end_number = gr.Textbox(visible=visible, value=str(end), label=f'Segment {i+1} End', interactive=True)
+
+                new_segment_group.extend([seg_textbox, start_number, end_number])
+            
+            return new_segment_group
+
+
+        new_segment_group = []
+        audio_amount = len(self.json_data)
+
+         # We make sure the user cannot try and go to before the beginning of audios
+        if index < 0:
+            index = 0  
+        elif index >= audio_amount:
+            index = audio_amount - 1  # If the user tries to go to a page out of range, he's drawn back to the last audio
         
-        if 0 <= index < entry_amount:
+        if 0 <= index < audio_amount:
             audio_file = get_audio_file()
             audio_name = os.path.basename(audio_file)
             audio_path = os.path.join(json_folder, 'audios', audio_file)
             audio_text = get_audio_text(audio_name)
-            current_page_label_label = f"Current Audio: {index + 1}/{entry_amount}"
+            curent_page_label = f"Current Audio: {index + 1}/{audio_amount}"
 
-            return audio_path, audio_name, index + 1, current_page_label_label, audio_text, ""
+
+            segments = self.json_data[audio_name]['segments']
+
+
+            for i in range(len(segments)):
+                new_segment_group.extend(create_segment_group(segments))
+
+            return audio_path, audio_name, index + 1, curent_page_label, audio_text, "", *new_segment_group
         
-        return None, "", 1, "Audio not available", "", ""
+        return None, "", 1, "Audio not available", "", "", *new_segment_group
             
             
 
