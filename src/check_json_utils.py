acc1864ca23934c1e9e3764aa58699a1b1d6dafb
Dr. Jusseaux <35258413+maepopi@users.noreply.github.com>
2024-02-25 19:21:20 +0100

    added comments to the JSON deletion function

--- src/check_json_utils.py
+++ src/check_json_utils.py
@@ -42,24 +42,32 @@ class AudioJsonHandler():
             json_file_path = os.path.join(json_folder, json_file)
             discarded_entries_path = os.path.join(json_folder, 'discarded_entries.json')
 
+            # Opening the JSON and loading its contents
             with open(json_file_path, 'r+') as file:
                 self.json_data = json.load(file)
 
+                # If there is only one entry in the JSON, return an error
                 if len(self.json_data) <= 1:
                     log_message = 'Cannot delete the last audio of the dataset'
                     return self.change_audio(index, json_folder, total_segment_components=total_segment_components, info_message=log_message)
 
+                # if the entry to delete is indeed in the data, store it in a variable and delete it from the JSON
                 if audio_name in self.json_data:
                     discarded_entry = self.json_data.pop(audio_name, None)
 
+                # Going back to the start of the file so that we can replace everything with the updated data
                 file.seek(0)
                 json.dump(self.json_data, file, indent=4)
+
+                # Truncating at the current position of the cursor so that no extra information is added by accident
                 file.truncate()
             
+            # If the discarded JSON entry file doesn't exist, create it and add the deleted entry
             if not os.path.exists(discarded_entries_path):
                 with open(discarded_entries_path, 'w') as discarded_json_file:
                     json.dump({audio_name : discarded_entry}, discarded_json_file, indent=4)
-                
+            
+            # If the discarded JSON entry file exists, oppen it and append the deleted entry
             else:
                 with open(discarded_entries_path, 'r+') as discarded_json_file:
                     discarded_json_data = json.load(discarded_json_file)
